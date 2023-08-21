0ab25a20784afd7c0467a3cc0d2e841c1ccf245e
Sergvah <104735864+Sergvah@users.noreply.github.com>
2023-08-21 11:29:32 +0300

    testoviy push

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <main>
       <h1>Local Low</h1>
-      <p style={{fontFamily: "Merriweather"}}>Welcome to app</p>
+      <p style={{ fontFamily: "Merriweather" }}>Welcome to app</p>
+      <p style={{ fontFamily: "Merriweather" }}>Test</p>
     </main>
-  )
+  );
 }
